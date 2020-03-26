77341fdfc6b117e53cc54a0dfc6435952a9388b7
Rodrigo Thaler Knolseisen <rtknolseisen@gmail.com>
2020-03-26 14:27:58 -0300

    improves team maker screen

---
+++ _site/js/manageTeam.js
@@ -0,0 +1,8 @@
+class manageTeam{
+  constructor(tableDOM, playersListDOM){
+    this.tableDOM = tableDOM;
+    this.playersListDOM = playersListDOM;
+    this.formation = '4-3-3';
+    this.selectedPlayers = []
+  }
+}
